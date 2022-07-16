80d7e908fea45e81e04d193de9ad48d8081b5da7
Dmytro <dima.plugaturevchik@gmail.com>
2022-07-16 22:50:12 +0300

    The main method was updated

--- Alphabet sort/main.cpp
+++ Alphabet sort/main.cpp
@@ -1,43 +1,9 @@
-﻿#include <iostream>
-#include"stringSorting.h"
-#include"stringValidation.h"
-using namespace std;
-
-string captureString();
+﻿#include"stringSorting.h"
+#include"capturer.h"
 
 int main()
 {
-	string toSort = captureString();
-	cout << "The sorted string is:" << endl << stringSort(toSort) << endl;
-
-	string toSortDescending = captureString();
-	cout << "The sorted descending string is:" << endl << stringSortDescending(toSortDescending);
-}
-
-string captureString()
-{
-	string result;
-	bool errorOccured;
-
-	do
-	{
-		errorOccured = false;
-
-		cout << "Please, enter a string which consists only of letters: ";
-		cin >> result;
-		
-		if (!validateOnlyLetters(result))
-		{
-			errorOccured = true;
-			cout << "The string includes not only letters" << endl;
-		}
-		else if (!validateOnlyRegister(result))
-		{
-			errorOccured = true;
-			cout << "The string's letters doesn't have the same register" << endl;
-		}
-	}
-	while (errorOccured);
-
-	return result;
+	string stringToSort = captureString();
+	cout << "The string which is sorted ascending:" << endl << stringSort(stringToSort) << endl;
+	cout << "The string which is sorted descending:" << endl << stringSortDescending(stringToSort);
 }
